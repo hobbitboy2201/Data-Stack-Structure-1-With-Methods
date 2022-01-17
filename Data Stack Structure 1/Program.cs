ae931bd030d912a559bc3ee58af458c878aaf21f
hobbi <hobbi@DESKTOP-SQCS301>
2022-01-17 22:30:12 +0000

    Started the program

--- Data Stack Structure 1/Program.cs
+++ Data Stack Structure 1/Program.cs
@@ -8,15 +8,24 @@ namespace Data_Stack_Structure_1
         static void Main(string[] args)
         {
             //Defines the main variables that will be used within the runtime of the program
-            Stack<int> Numbers = new Stack<int>();
-            Numbers.Push(3);
+            Stack<int> Numbers = new Stack<int>(); //Defines a new Stack<int> variable called 'Numbers'
 
-            //Top
+            //Push - Inserts an object at the top of the stack
 
-            //Pop
+            Numbers.Push(5); //Adds the integer '5' to the top of the Stack 'Numbers'
+            Numbers.Push(9); //Adds the integer '9' to the top of the Stack 'Numbers'
 
-            //Push
-            Numbers.Push(3);
+            Console.WriteLine(); //Empty WritLine command to make the console look neater 
+
+            //Peek - Returns the object at the top of the 
+            Console.WriteLine("Peek:");
+            Console.WriteLine(Numbers.Peek());
+
+            Console.WriteLine(); //Empty WritLine command to make the console look neater 
+
+            //Pop - Returns and removes the object at the top of the stack
+            Console.WriteLine("Pop:");
+            Console.WriteLine(Numbers.Pop());
         }
     }
 }
