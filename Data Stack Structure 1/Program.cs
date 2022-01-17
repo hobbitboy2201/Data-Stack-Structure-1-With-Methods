3621443c257b8309ea0a61b21c0308086a4ea68b
hobbi <hobbi@DESKTOP-SQCS301>
2022-01-17 20:35:14 +0000

    Add project files.

---
+++ Data Stack Structure 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data_Stack_Structure_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Defines the main variables that will be used within the runtime of the program
+            Stack<int> Numbers = new Stack<int>();
+            Numbers.Push(3);
+
+            //Top
+
+            //Pop
+
+            //Push
+        }
+    }
+}
